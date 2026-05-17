98d79351fb0d0803fd24543ba79294682576f43f
AlexandruUCV <gogosanu.alexandru.i3a@student.ucv.ro>
2026-05-17 12:09:39 +0300

    Buzzer

--- src/main.c
+++ src/main.c
@@ -1,30 +1,26 @@
 #include <stdint.h>
-
 #include "drivers/gpio/gpio.h"
 #include "drivers/pwm/pwm.h"
 #include "drivers/adc/adc.h"
-
 #include "bsp/nano.h"
 #include "utils/delay.h"
 
-// LED-uri
 #define LED_GREEN   D11
 #define LED_YELLOW  D9
 #define LED_RED     D10
-
-// Termistor pe ADC0
+#define BUZZER D6
 #define TEMP_CHANNEL 0
 
 uint16_t temp_value;
 
-/* praguri simple (calibratezi tu) */
+// praguri
 #define COLD_THRESHOLD   500
 #define HOT_THRESHOLD    560
 
 int main(void)
 {
     ADC_Init();
-
+    GPIO_Init(BUZZER, GPIO_OUTPUT);
     PWM_Init(LED_GREEN, 1000);
     PWM_Init(LED_YELLOW, 1000);
     PWM_Init(LED_RED, 1000);
@@ -32,13 +28,11 @@ int main(void)
     while(1)
     {
         temp_value = ADC_Read(TEMP_CHANNEL);
-
-        // reset LED-uri
+        GPIO_Write(BUZZER, 0);
         PWM_SetDutyCycle(LED_GREEN, 0);
         PWM_SetDutyCycle(LED_YELLOW, 0);
         PWM_SetDutyCycle(LED_RED, 0);
 
-        // LOGICĂ SIMPLĂ
         if(temp_value < COLD_THRESHOLD)
         {
             PWM_SetDutyCycle(LED_GREEN, 255);
@@ -50,8 +44,8 @@ int main(void)
         else
         {
             PWM_SetDutyCycle(LED_RED, 255);
+            GPIO_Write(BUZZER, 1);
         }
-
         Delay(200);
     }
 }
